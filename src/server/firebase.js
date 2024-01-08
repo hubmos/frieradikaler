b19a1a416616b2262909e6c3626fd86bdfb9003d
hubmos <45960327+hubmos@users.noreply.github.com>
2024-01-08 09:12:53 +0100

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -2,8 +2,10 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
 // Initialize Firebase Admin SDK
+const jsonk= await import.meta.env.SECRET_ACCOUNT;
+const keys = JSON.parse(jsonk);
 admin.initializeApp({
- credential: admin.credential.cert(JSON.parse(import.meta.env.SECRET_ACCOUNT))});
+ credential: admin.credential.cert(keys)});
 };
 // Initialize Firestore Database
 const db = admin.firestore();
