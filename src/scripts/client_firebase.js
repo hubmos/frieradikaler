fb79ec4a3a8e0d557a46724ad14024934724a3fa
Eivind desktop <eivind.sundberg@hotmail.com>
2024-01-14 10:43:56 +0100

    test

--- src/scripts/client_firebase.js
+++ src/scripts/client_firebase.js
@@ -17,26 +17,34 @@ const firebaseConfig = {
 
 const app=initializeApp(firebaseConfig);
 
+  document.addEventListener('click', function handleNotificationRequest() {
+    Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+            navigator.serviceWorker.register('/frieradikaler/firebase-messaging-sw.js')
+                .then((registration) => {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                    const messaging = getMessaging(app);
+                    return getToken(messaging, { 
+                        vapidKey: 'BETQHChUvvFIizz6vL043qT1BXRpXwWOr1gqc6LyLgi4fnm1UMThR_Nvvu3OfE5prGOz85TavHU5vMZe-2q5ztw', 
+                        serviceWorkerRegistration: registration
+                    });
+                })
+                .then((currentToken) => {
+                    if (currentToken) {
+                        console.log('success', currentToken);
+                        // Additional logic here
+                    } else {
+                        console.log('No registration token available. Request permission to generate one.');
+                    }
+                })
+                .catch((err) => {
+                    console.log('An error occurred while retrieving token. ', err);
+                });
+        } else {
+            // Handle denied permission
+        }
+    });
 
-let permission = await Notification.requestPermission();
-if (permission === "granted") {
-// Register Service Worker
-navigator.serviceWorker.register('/frieradikaler/firebase-messaging-sw.js')
-  .then((registration) => {
-    console.log('Service Worker registered with scope:', registration.scope);
-    const messaging = getMessaging(app);
-    return getToken(messaging, { vapidKey: 'BETQHChUvvFIizz6vL043qT1BXRpXwWOr1gqc6LyLgi4fnm1UMThR_Nvvu3OfE5prGOz85TavHU5vMZe-2q5ztw', serviceWorkerRegistration: registration}, );
-  })
-  .then((currentToken) => {
-    if (currentToken) {
-      console.log('success', currentToken);
-      // Send the token to your server and update the UI if necessary
-    } else {
-      console.log('No registration token available. Request permission to generate one.');
-    }
-  })
-  .catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-  });} else {
-    // Handle denied permission
-  }
+    // Remove the event listener after handling the request
+    document.removeEventListener('click', handleNotificationRequest);
+});
