d7a265d28185f16dcae77ef3c3d1c045744e055e
hubmos <45960327+hubmos@users.noreply.github.com>
2024-02-09 10:08:42 +0100

    Update Bar.jsx

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -98,7 +98,7 @@ const ActivitiesCharts = ({ acts }) => {
 
   return (
     <div>
-      <select className="select select-bordered select-sm w-full max-w-xs justify-self-center" onChange={handleActivityChange}>
+      <select className="select select-bordered select-sm w-full max-w-xs justify-self-center content-center" onChange={handleActivityChange}>
         <option value="Totalt">Totalt</option>
         <option value="Sykkel">Sykkel</option>
         <option value="Løping">Løping</option>
